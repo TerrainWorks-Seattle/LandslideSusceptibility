4b89db3768a97b0eb9ea22a1b2a52c81fd73529c
daniellrgn <tatebrasel@gmail.com>
2021-11-15 14:37:23 -0800

    rename estimateConfidenceIntervals.R to ConfidenceIntervals.R

--- ConfidenceIntervals.R
+++ ConfidenceIntervals.R
@@ -24,11 +24,12 @@ estimateConfidenceIntervals <- function(
   
   # Load helper functions ------------------------------------------------------
   
-  source("./shared/createInitiationRange.R")
-  source("./shared/createAnalysisRegionMask.R")
-  source("./shared/generateNoninitiationBuffers.R")
-  source("./shared/extractBufferValues.R")
-  source("./shared/createProportionRaster.R")
+  source("./helper/createInitiationRange.R")
+  source("./helper/createAnalysisRegionMask.R")
+  source("./helper/generateNoninitiationBuffers.R")
+  source("./helper/extractBufferValues.R")
+  source("./helper/createProportionRaster.R")
+  source("./helper/createConfidenceIntervalRaster.R")
   
   # Validate parameters --------------------------------------------------------
   
@@ -224,25 +225,6 @@ estimateConfidenceIntervals <- function(
   
   terra::writeRaster(probConfidenceRaster, paste0(outputDir, "/probconf.tif"))
   terra::writeRaster(propConfidenceRaster, paste0(outputDir, "/propconf.tif"))
-}
-
-createConfidenceIntervalRaster <- function(x, p = 0.05) {
-  n <- terra::nlyr(x)
-  degreesFreedom <- n - 1
-  tScore <- qt(p = p, df = degreesFreedom, lower.tail = FALSE)
-  
-  sd <- terra::app(x, fun = "sd") # Standard deviation
-  se <- sd / sqrt(n)              # Standard error
-  me <- se * tScore               # Margin of error
-  
-  mean <- terra::mean(x)          # Mean
-  lower <- mean - me              # Interval lower bound
-  upper <- mean + me              # Interval upper bound
-  
-  interval <- c(lower, upper)
-  names(interval) <- c("lower", "upper")
-  
-  return(interval)
 }
 
 if (FALSE) {
